c70c829e03bf4cf0e4928ef6d028974ac9f7e214
TAKEDA HAJIME <a0129169@ws41.(none)>
2015-01-15 16:49:26 +0900

    kadai3途中

--- server1.2/myclient.c
+++ server1.2/myclient.c
@@ -9,30 +9,30 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
-#define BUF_LEN 256                      /* バッファのサイズ */
+#define BUF_LEN 1024                      // バッファのサイズ 
 
 int main(int argc, char *argv[]){
   int s;  /* ソケットのためのファイルディスクリプタ */
   int client_id;
   size_t goods_num,client_num;
-  struct hostent *servhost;            /* ホスト名と IP アドレスを扱うための構造体 */
-  struct sockaddr_in server;           /* ソケットを扱うための構造体 */
-  struct servent *service;             /* サービス (http など) を扱うための構造体 */
+  struct hostent *servhost;            // ホスト名と IP アドレスを扱うための構造体 
+  struct sockaddr_in server;           // ソケットを扱うための構造体 
+  struct servent *service;             // サービス (http など) を扱うための構造体 
   char buf[BUF_LEN];
   int read_size;
 
   char *host;
-  //char host[BUF_LEN] = "127.0.0.1";    /* 接続するホスト名 */
-  char path[BUF_LEN] = "/";            /* 要求するパス */
-  unsigned short port = 5000;             /* 接続するポート番号 */
+  //char host[BUF_LEN] = "127.0.0.1";    // 接続するホスト名 
+  char path[BUF_LEN] = "/";            // 要求するパス 
+  unsigned short port = 5000;             // 接続するポート番号 
 
-  /* 引数が不足している場合 */
+  // 引数が不足している場合 
   if (argc < 3) {
     printf("usage: client [hostname] [port]\n");
     exit(1);
   }
 
-  /* コマンドライン引数からサーバ名を取得 */
+  // コマンドライン引数からサーバ名を取得
   if (argc > 1) {
     host = (char *)argv[1];
   }
@@ -41,40 +41,60 @@ int main(int argc, char *argv[]){
     port = atoi(argv[2]);
   }
 
-  /* ホストの情報(IPアドレスなど)を取得 */
+  // ホストの情報(IPアドレスなど)を取得 
   servhost = gethostbyname(host);
   if ( servhost == NULL ){
     fprintf(stderr, "[%s] から IP アドレスへの変換に失敗しました。\n", host);
     return 0;
   }
 
-  bzero(&server, sizeof(server));            /* 構造体をゼロクリア */
+  bzero(&server, sizeof(server));            // 構造体をゼロクリア 
 
   server.sin_family = AF_INET;
 
-  /* IPアドレスを示す構造体をコピー */
+  // IPアドレスを示す構造体をコピー 
   bcopy(servhost->h_addr, &server.sin_addr, servhost->h_length);
 
   server.sin_port = htons(port);
 
-  /* ソケット生成 */
+  // ソケット生成 
   if ( ( s = socket(AF_INET, SOCK_STREAM, 0) ) < 0 ){
     fprintf(stderr, "ソケットの生成に失敗しました。\n");
     return 1;
   }
-  /* サーバに接続 */
+  // サーバに接続 
   if ( connect(s, (struct sockaddr *)&server, sizeof(server)) == -1 ){
     fprintf(stderr, "connect に失敗しました。\n");
     return 1;
   }
-  //PLEASE INPUT YOUR NAME
-  /* 名前の送信 */
+
+  // 名前の送信 
   read_size = read(s, buf, BUF_LEN);//sにつながっているファイルからBUF_LENバイト読み込みbuf格納
   if ( read_size > 0 ){
     write(1, buf, read_size);
   }
-   fgets(buf, BUF_LEN, stdin);
-    write(s, buf, strlen(buf));
+
+  printf("ここでサーバーからPLEASE INPUT YOUR NAMEと表示される.名前を入力してエンター");
+  //標準入力を使わずにサーバーに名前(randomな１０桁の数字)を送信
+  int i=0;
+  /* for (i=0;i<10;i++){
+    buf=rand() %10000;
+    printf("buf[%d]=%d",i,buf[i]);
+  }
+  buf[i] = '\n';
+  printf("buf[%d]=%d",i,buf[i]);
+  buf[i+1] = '\0';
+  printf("buf[%d]=%d",i+1,buf[i+1]);
+  printf("最初の自動入札（全てに1=入札）");  
+  */
+  // buf=rand() %10000;
+  char str;
+  sprintf(str, "%d", rand() %10000);
+
+   fgets(buf, BUF_LEN, str);
+  //  fgets(buf, BUF_LEN, stdin);
+  printf("名前送信された。全てのクライアントが入力し終わる同時にyour id:とクライアント数,商品数表示された\n");
+  write(s, buf, strlen(buf));
 
 
 
@@ -91,7 +111,8 @@ int main(int argc, char *argv[]){
  
 
   // "Your ID: x", x はクライアントID
-  if ((read_size = read(s, buf, BUF_LEN)) > 0)
+  read_size = read(s, buf, BUF_LEN);
+  if ( read_size> 0)
     write(1, buf, read_size);
   buf[read_size] = '\0';
   sscanf(buf, "%*[^0-9]%d", &client_id);
@@ -99,7 +120,7 @@ int main(int argc, char *argv[]){
   //sscanfは要はscanfの入力がキーボードからではなく文字列strになる
   //sscanf(const char *str入力元の文字列, const char *format書式指定文字列,格納する引数)
   //0-9までの%d(10進数int型)
-  //^はYOUR ID:に相当し、捨てる
+  //^は"YOUR ID:"に相当し、捨てる
 
   // "n,m", n は商品数 m はクライアント合計数
   if ((read_size = read(s, buf, BUF_LEN)) > 0)
@@ -111,54 +132,79 @@ int main(int argc, char *argv[]){
 
 
 
-  puts("入札情報を入力");
-
+  puts("最初の入札情報を自動で入力（全て１）");
+  
   //それぞれの商品について入札するかどうか。各商品に対して入札する場合は1，しない場合は0
   
   //標準入力でサーバーに情報を送信
-  // fgets(buf, BUF_LEN, stdin);
+  //fgets(buf, BUF_LEN, stdin);
 
   //標準入力を使わずにサーバーに情報を送信
-   int i=0;
+ 
   for (i=0;i<goods_num;i++){
-  buf[i] = '1';
+    buf[i] = '1';
+    printf("buf[%d]=%d",i,buf[i]);
   }
-   buf[i] = '\n';
-   buf[i+1] = '\0';
-   printf("最初の入札");
-
-  write(s, buf, strlen(buf));//ソケットにつながるsにbufをstrlenバイト書き込む
+  buf[i] = '\n';
+  printf("buf[%d]=%d",i,buf[i]);
+  buf[i+1] = '\0';
+  printf("buf[%d]=%d",i+1,buf[i+1]);
+  printf("最初の自動入札（全てに1=入札）");
   
-
+  write(s, buf, strlen(buf));//ソケットにつながるsにbufをstrlenバイト書き込む
+  //  printf("yyyyyyyyyyyyyyyyyyyyyyyyyyyy");
+   
   read_size = read(s, buf, BUF_LEN);
   if ( read_size > 0 ){
-    write(1, buf, read_size);
+    write(1, buf, 
+	  read_size);
   }
+  //printf("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
 
-  /* IDとクライアント数、商品数の読み込み */
-  read_size = read(s, buf, BUF_LEN);
-  if ( read_size > 0 ){
-    write(1, buf, read_size);
-  }
-
-  read_size = read(s, buf, BUF_LEN);
-  if ( read_size > 0 ){
-    write(1, buf, read_size);
-  }
-
+  printf("最初の入札として全てに１が入力された。");
   /* 送受信ループ */
   /* 標準入力をそのまま送信、入札結果と価格を受信して表示 */
   while (1) {
-    fgets(buf, BUF_LEN, stdin);
-    write(s, buf, strlen(buf));
+    printf("press enter?");
+    //ここで標準入力ではなく代入する。
+    char str[256];
+    fgets(str, 256, stdin);
+    printf("str:%s",str);
+    //  if(){
+      int i=0;
+      for (i=0;i<goods_num;i++){
 
+
+   
+	if(rand()%2==0){
+	  buf[i] = '0';
+	  printf("rand mod 2==0");
+	}else if(rand()%2==1){
+	  buf[i] = '1';
+	  printf("rand mod 1==0");
+	}else{
+	  printf("oops:%d:",rand()%2);
+	}
+	printf("buf[%d]=%d",i,buf[i]);
+      }
+      buf[i] = '\n';
+      printf("buf[%d]=%d",i,buf[i]);
+      buf[i+1] = '\0';
+      printf("buf[%d]=%d",i+1,buf[i+1]);
+      //  }
+
+    write(s, buf, strlen(buf));
+    printf("2222222222222222222222222222222222");
     read_size = read(s, buf, BUF_LEN);
     if ( read_size > 0 ){
       write(1, buf, read_size);
     }
+    printf("33333333333333333333333333333333");
     read_size = read(s, buf, BUF_LEN);
+    printf("444444444444444444444444444444");
     if ( read_size > 0 ){
       write(1, buf, read_size);
+      printf("ここで次の入力待ち");
     }
   }
 
